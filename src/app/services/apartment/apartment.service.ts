f56ad2121795c212ce62159819c960116d3c81ae
AlinaHrypa <alina.hrypa@gmail.com>
2018-08-23 13:53:00 +0300

    edited create apartment

--- src/app/services/apartment/apartment.service.ts
+++ src/app/services/apartment/apartment.service.ts
@@ -5,31 +5,37 @@ import {Apartment} from '../../models/apartment';
 import {tap} from 'rxjs/operators';
 import {ApartmentType} from '../../models/apartment-type';
 import {Amenity} from '../../models/amenity';
+import {CreateApartment} from '../../models/create-apartment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApartmentService {
+  // private apartmentUrl = '/api/property/';
   private apartmentUrl = '/api/property/1/apartment';
   private typeUrl = '/api/create-apartment/apartment-type';
   private amenityUrl = '/api/create-apartment/amenities';
 
   constructor(private http: HttpClient) { }
 
+  // public getAllApartments(propertyId: number): Observable<Apartment[]> {
+  //   return this.http.get<Apartment[]>(`${this.apartmentUrl}${propertyId}/apartment`);
+  // }
+
   public getAllApartments(): Observable<Apartment[]> {
     return this.http.get<Apartment[]>(this.apartmentUrl);
-  }
-
-  public getApartmentType(): Observable<ApartmentType[]> {
-    return this.http.get<ApartmentType[]>(this.typeUrl);
   }
 
   public getAmenities(): Observable<Amenity[]> {
     return this.http.get<Amenity[]>(this.amenityUrl);
   }
 
-  public createApartment(apartment: Apartment) {
-    return this.http.post(this.apartmentUrl, apartment,
+  public getApartmentType(): Observable<ApartmentType[]> {
+    return this.http.get<ApartmentType[]>(this.typeUrl);
+  }
+
+  public createApartment(createApartment: CreateApartment) {
+    return this.http.post(this.apartmentUrl, createApartment,
       {headers: new HttpHeaders({
           'Content-Type': 'application/json' }),
       responseType: 'text'
