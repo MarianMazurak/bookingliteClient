86d8a8cfc9c077c0217c0281ca79210aec818793
Roman Mozil <Mozil_r@ukr.net>
2018-08-26 16:19:48 +0300

    some chenges to pages

--- src/app/services/booking/booking.service.ts
+++ src/app/services/booking/booking.service.ts
@@ -11,27 +11,23 @@ import {Booking} from '../../models/booking';
 })
 export class BookingService {
 
-  private bookingUrl = 'api/bookings'
-  private bookingUrl2 = 'api/booking'
+  private bookingsUrl = 'api/bookings'
+  private bookingUrl = 'api/booking'
   private nowDate: Date;
 
   constructor(private http: HttpClient) { }
 
   getBookings(): Observable<Booking[]> {
-    return this.http.get<Booking []>(this.bookingUrl)
-      .pipe(tap(res => console.log('catch bookings')
-      ));
+    return this.http.get<Booking []>(this.bookingsUrl);
   }
 
   getBooking(id: number): Observable<Booking> {
-    const url = `${this.bookingUrl2}/${id}`;
-    return this.http.get<Booking>(url)
-    .pipe(tap(res => console.log('catch ONE booking')
-      ));
+    const url = `${this.bookingUrl}/${id}`;
+    return this.http.get<Booking>(url);
   }
 
   cancelBookings(id: number) {
-    const url = `${this.bookingUrl2}/${id}`;
+    const url = `${this.bookingUrl}/${id}`;
     return this.http.put(url, 
       {headers: new HttpHeaders({
       'Content-Type':  'application/json' })
@@ -43,11 +39,9 @@ export class BookingService {
   isCanceled( bookingStatus: string):boolean {
     const canceledStatus = 'Canceled';
     if(bookingStatus == canceledStatus){
-     //console.log('iscancel booking TRUE');  
       return true;
     }
     else{
-      //console.log('iscancel booking FALSE');  
       return false;
     }
   }
@@ -63,16 +57,10 @@ export class BookingService {
     let dateChackIn = new Date (checkIn);
     let dateChackOut = new Date (checkOut);
     this.nowDate = new Date();
-    //console.log('date', d);
-    //console.log('this.today', this.nowDate);
-    //console.log('date1', checkIn);
-    //console.log('date2', checkOut);
     if(dateChackIn > this.nowDate || dateChackOut > this.nowDate){
-      //console.log('isCheckBookingDate', true);
       return true;
     }
     else {
-      //console.log('isCheckBookingDate', false);
       return false;
     }
   }
