f158e30baeed3049566fb2669ab12598521665ff
AlinaHrypa <alina.hrypa@gmail.com>
2018-08-26 17:37:17 +0300

    Marian's property created

--- src/app/services/review/review.service.ts
+++ src/app/services/review/review.service.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {Property} from '../../models/property';
 import {Apartment} from '../../models/apartment';
+import {User} from '../../models/user';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,5 +17,9 @@ export class ReviewService {
 
   public getAllReviews(id: number): Observable<Review[]> {
     return this.http.get<Review[]>(`${this.reviewUrl}/${id}/reviews`);
+  }
+
+  public getUserDetails(): Observable<User> {
+    return this.http.get<User>(`/api/hello`);
   }
 }
