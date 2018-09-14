919ec99e57093d7dbc435b758523ff7002c62855
YaroslavRenhevych <41372491+YaroslavRenhevych@users.noreply.github.com>
2018-09-14 15:48:37 +0300

    add params with data to routerLinks (#46)

--- src/app/components/property/list-property/list-property.component.ts
+++ src/app/components/property/list-property/list-property.component.ts
@@ -1,10 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 import {Property} from '../../../models/property';
-import {PropertyService} from '../../../services/property/property.service';
-import {AuthService} from '../../../services/authentication/auth.service';
-import {ReviewService} from '../../../services/review/review.service';
-import {ApartmentService} from '../../../services/apartment/apartment.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-list-property',
@@ -13,17 +10,17 @@ import {ApartmentService} from '../../../services/apartment/apartment.service';
 })
 export class ListPropertyComponent implements OnInit {
 
-  private authentication;
+  @Input() propertyList: Property[]
+  public queryParams = {
+    checkin: '',
+    checkout: '',
+    num_of_guests: ''
+  }
+  constructor(private route: ActivatedRoute) {}
 
-  @Input() propertyList: Property[];
-  constructor(private propertyService: PropertyService,
-              private reviewService: ReviewService,
-              private apartmentService: ApartmentService,
-              private auth: AuthService ) {}
-
-
-  reviewCount: number;
   ngOnInit() {
-    this.authentication = this.auth.isAuthenticated;
+    this.queryParams.checkin = this.route.snapshot.queryParamMap.get('checkin');
+    this.queryParams.checkout = this.route.snapshot.queryParamMap.get('checkout');
+    this.queryParams.num_of_guests = this.route.snapshot.queryParamMap.get('num_of_guests');
   }
  }
